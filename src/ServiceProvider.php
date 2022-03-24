81ce48d472b9559e8194e4ec843f0c5c08fd1cee
15620980958 <rebing0512@aliyun.com>
2022-03-24 16:34:09 +0800

    commit

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,17 +10,15 @@ class ServiceProvider extends BaseServiceProvider
     public function boot()
     {
 
-        // 【2】路由
+        // 路由
         $this->setupRoutes($this->app->router);
         // 配置
         $this->mergeConfigFrom(
             __DIR__ . '/config/mbcore_ueditor.php', 'mbcore_ueditor',
-            __DIR__ . '/config/mbcore_ueditor_config.php', 'mbcore_ueditor_config'
         );
         // 发布配置文件
         $this->publishes([
             __DIR__.'/config/mbcore_ueditor.php' => config_path('mbcore_ueditor.php'),
-            __DIR__.'/config/mbcore_ueditor_config.php' => config_path('mbcore_ueditor_config.php'),
         ], 'config');
 
         // 资源文件
@@ -45,7 +43,6 @@ class ServiceProvider extends BaseServiceProvider
     {
         config([
             'config/mbcore_ueditor.php',
-            'config/mbcore_ueditor_config.php',
         ]);
     }
 }
