f931bc08ad88faf4b29c4c3faf93d8536f4c14bb
15620980958 <rebing0512@aliyun.com>
2022-03-24 15:22:25 +0800

    commit

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,26 +7,21 @@ class ServiceProvider extends Controllers
 
     public function boot()
     {
-
-
-
-        // 【3】配置
+        // 配置
         $this->mergeConfigFrom(
             __DIR__ . '/config/mbcore_ueditor.php', 'mbcore_ueditor'
         );
-        //发布配置文件
+        // 发布配置文件
         $this->publishes([
             __DIR__.'/config/mbcore_ueditor.php' => config_path('mbcore_ueditor.php'),
         ], 'config');
 
 
-        // 【5】资源文件
+        // 资源文件
         $this->publishes([
             __DIR__.'/resources/assets' => public_path('assets/Jenson/Ueditor'),
         ], 'public');
-
     }
-
 
     /**
      * Register the application services.
